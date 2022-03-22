aa76efc62c3a78aa27683cf4a39e37c575ab0116
zhangkai8048 <wode804847802@163.com>
2022-03-22 14:30:25 +0800

    add user

--- config/samples/elasticsearch/docker.sh
+++ config/samples/elasticsearch/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -v $(pwd)/filerealm:/usr/share/elasticsearch/config docker.elastic.co/elasticsearch/elasticsearch:7.10.2 bin/elasticsearch-users useradd kubesphere -p kubesphere -r superuser
-kubectl create secret generic kubesphere-elasticsearch-realm-secret --from-file filerealm -n kubesphere-logging-system
+docker run -v $(pwd)/filerealm:/usr/share/elasticsearch/config docker.elastic.co/elasticsearch/elasticsearch:7.10.2 bin/elasticsearch-users useradd bocloud -p bocloud -r superuser
+kubectl create secret generic bocloud-elasticsearch-realm-secret --from-file filerealm 
